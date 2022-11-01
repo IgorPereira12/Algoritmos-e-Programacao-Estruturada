c66f981a72bcb4060a8370d4ba351bfbcb4efc1d
Xumbrunha <igorpereira_712@hotmail.com>
2022-11-01 01:29:36 -0300

    Exemplos Aula 3

---
+++ Aula 3/Exemplo 2/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void inserir(int a[]){ 
+    int i=0; 
+    for(i=0;i<3;i++){ 
+        printf("Digite o valor %d: ",i); 
+        scanf("%d",&a[i]); 
+        
+    } 
+}
+
+void imprimir(int b[]){ 
+    int i=0; 
+    for(i=0;i<3;i++){ 
+        printf("\n numeros[%d] = %d",i,2*b[i]); 
+        
+    } }
+        
+int main(){
+    int numeros[3];
+    
+    printf("\n Preenchendo o vetor... \n "); 
+    inserir(numeros);
+    
+    printf("\n Dobro dos valores informados:"); 
+    imprimir(numeros);
+    return 0; 
+}
+
