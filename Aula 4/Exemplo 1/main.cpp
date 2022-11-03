062d15870d2077762337b20be5cd05b01731b379
Xumbrunha <igorpereira_712@hotmail.com>
2022-11-03 14:56:11 -0300

    Exemplos Aula 4

---
+++ Aula 4/Exemplo 1/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct Node 
+{ 	
+  int dado; 
+  struct Node *next; 
+}; 
+  
+void insere_inicio(struct Node** inicio_ref, int new_dado) 
+{ 
+    
+
+} 
+  
+
+void insere_meio(struct Node* prev_node, int new_dado) 
+{ 
+
+} 
+  
+void insere_fim(struct Node** inicio_ref, int new_dado) 
+{ 
+       
+} 
+  
+void printList(struct Node *node) 
+{ 
+  
+} 
+  
+int main() 
+{ 
+  struct Node* inicio = NULL; 
+  
+  insere_fim(&inicio, 6); 
+  
+  insere_inicio(&inicio, 7); 
+  
+  insere_inicio(&inicio, 1); 
+  
+  insere_fim(&inicio, 4); 
+  
+  insere_meio(inicio->next, 8); 
+  
+  printf("\n Lista Criada: "); 
+  printList(inicio); 
+  
+  return 0; 
+}
+
+
